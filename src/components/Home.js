e9e5a7a32e6ace0848b418d546fae6055dec6d3e
mjako <jakobmelendez@ufl.edu>
2022-02-01 14:47:07 -0500

    entire portfolio template

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from "react"
+import image from "../tpa.jpeg"
+
+export default function Home() {
+    return (
+        <main>
+            <img src={ image } alt="Tampa Bay Skyline at Night" className="absolute object-cover w-full h-full"/>
+            <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+                <h1 className="text-6xl text-green-100 font-bold sans-serif leading-none lg:leading-snug home-name">Hi! I'm Jakob.</h1>
+            </section>
+        </main>
+    )
+}
